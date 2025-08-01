3824058e003d7b16a0cca53775f6e74cfc648fe8
wonyoungE <error071312@gmail.com>
2025-08-01 16:55:54 +0900

    feat: 달력 완성

---
+++ todo_calendar/script.js
@@ -0,0 +1,94 @@
+const calendar = document.querySelector("#calendar-days");
+const weekdays = document.querySelector("#weekdays");
+const days = document.querySelector("#days");
+const yearMonth = document.querySelector("#year-month");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+
+let date = new Date();
+let currentYear = date.getFullYear();
+let currentMonth = date.getMonth();
+
+function renderCalendar(year, month) {
+  weekdays.innerHTML = "";
+  days.innerHTML = "";
+
+  // 요일 -> 0(일)부터 시작 | 월 -> 0(1월)부터 시작
+  const firstDay = new Date(year, month, 1).getDay(); // 0(일) ~ 6(토)
+  const lastDate = new Date(year, month + 1, 0).getDate(); // 말일 -> 다음 달의 0일로 설정
+  const lastDay = new Date(year, month, lastDate).getDay();
+
+  yearMonth.innerText = `${year}년 ${month + 1}월`;
+
+  // 요일
+  const weekdaysList = ["일", "월", "화", "수", "목", "금", "토"];
+
+  weekdaysList.forEach((day) => {
+    const dayDiv = document.createElement("div");
+    dayDiv.classList.add("weekday");
+    dayDiv.innerText = day;
+    weekdays.appendChild(dayDiv);
+  });
+
+  // 날짜
+  // 해당 월의 1일 요일 전까지 div 채우기
+  for (let i = 0; i < firstDay; i++) {
+    const empty = document.createElement("div");
+    empty.classList.add("day");
+    days.appendChild(empty);
+  }
+
+  // 1일~말일까지 날짜 채우기
+  for (let i = 1; i <= lastDate; i++) {
+    const dayDiv = document.createElement("div");
+    dayDiv.classList.add("day");
+    dayDiv.textContent = i;
+
+    // 오늘이면
+    const today = new Date();
+
+    if (
+      year === today.getFullYear() &&
+      month === today.getMonth() &&
+      i === today.getDate()
+    ) {
+      dayDiv.classList.add("today");
+    }
+
+    days.appendChild(dayDiv);
+  }
+
+  // 달력 크기 고정을 위해 달력의 칸 수를 42칸으로 맞추기
+  // 0: 일요일
+  const curDivs = days.children.length;
+  console.log(curDivs);
+  for (let i = curDivs; i < 42; i++) {
+    const empty = document.createElement("div");
+    empty.classList.add("day");
+    days.appendChild(empty);
+  }
+}
+
+prevBtn.addEventListener("click", () => {
+  if (currentMonth == 0) {
+    currentYear--;
+    currentMonth = 11;
+  } else {
+    currentMonth--;
+  }
+
+  renderCalendar(currentYear, currentMonth);
+});
+
+nextBtn.addEventListener("click", () => {
+  if (currentMonth == 11) {
+    currentYear++;
+    currentMonth = 0;
+  } else {
+    currentMonth++;
+  }
+
+  renderCalendar(currentYear, currentMonth);
+});
+
+renderCalendar(currentYear, currentMonth);
